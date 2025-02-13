a6140328e1e4216a66427c7302ac6cd4a9a1e170
daisuke <daisuke.imahase@gmail.com>
2025-02-13 22:32:58 +0900

    ENDPOINT→URL

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -9,7 +9,7 @@ export default function Home() {
   const handlePostRequest = async () => {
     try{
       //const response = await fetch(`http://127.0.0.1:8000/api/read?itemCode=${itemCode}`, {
-      const response = await fetch(`${process.env.NEXT_PUBLIC_API_ENDPOINT}/api/read?itemCode=${itemCode}`, {
+      const response = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/api/read?itemCode=${itemCode}`, {
       method: 'GET',
       headers: {
         'Content-Type': 'application/json',
@@ -42,7 +42,7 @@ export default function Home() {
 
     try{
       // const response = await fetch(`http://127.0.0.1:8000/api/purchase`, {
-      const response = await fetch(`${process.env.NEXT_PUBLIC_API_ENDPOINT}/api/purchase`, {
+      const response = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/api/purchase`, {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json',
