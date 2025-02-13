907efa66599a5e4b35a7b0594f70dade81e9f291
daisuke <daisuke.imahase@gmail.com>
2025-02-13 22:16:54 +0900

    page.jsxのつなぎ先をローカルから変更

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -8,8 +8,8 @@ export default function Home() {
 
   const handlePostRequest = async () => {
     try{
-      const response = await fetch(`http://127.0.0.1:8000/api/read?itemCode=${itemCode}`, {
-      // const response = await fetch(`${process.env.NEXT_PUBLIC_API_ENDPOINT}/api/read?itemCode=${itemCode}`, {
+      //const response = await fetch(`http://127.0.0.1:8000/api/read?itemCode=${itemCode}`, {
+      const response = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/api/read?itemCode=${itemCode}`, {
       method: 'GET',
       headers: {
         'Content-Type': 'application/json',
@@ -41,8 +41,8 @@ export default function Home() {
     }
 
     try{
-      const response = await fetch(`http://127.0.0.1:8000/api/purchase`, {
-      // const response = await fetch(`${process.env.NEXT_PUBLIC_API_ENDPOINT}/api/purchase`, {
+      // const response = await fetch(`http://127.0.0.1:8000/api/purchase`, {
+      const response = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/api/purchase`, {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json',
